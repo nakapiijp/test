ab1959c8ed38bbbde8d8b8717d35ee5c7995468a
naka <naka@DESKTOP-U0T8T71>
2020-11-01 22:01:09 +0900

    update

--- test01/src/test/java/test01/LibraryTest.java
+++ test01/src/test/java/test01/LibraryTest.java
@@ -12,5 +12,8 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         System.out.println("123789");
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        System.out.println("123789");
+        System.out.println("456");
+        System.out.println("123789");
     }
 }
