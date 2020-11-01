c7bb1593ec75d9548ca2fa9fdf7494c3802d6f41
nakapiijp <69384037+nakapiijp@users.noreply.github.com>
2020-11-01 15:13:41 +0900

    Update LibraryTest.java
    update

--- test01/src/test/java/test01/LibraryTest.java
+++ test01/src/test/java/test01/LibraryTest.java
@@ -11,6 +11,6 @@ public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-        System.out.println("123");
+        System.out.println("123456");
     }
 }
